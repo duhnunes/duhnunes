d6212e4c4a73c11c87884a7dc8246d3b9aa86e39
duhnunes <duhnunes.dev@gmail.com>
2025-03-28 21:49:34 -0300

    doc: Add function to terminal open RecycleBin

--- windows/Ubuntu(WSL2)/config.fish
+++ windows/Ubuntu(WSL2)/config.fish
@@ -2,19 +2,25 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Custom Functions
+
+function recicle_bin_folder
+  cmd.exe /c start shell:RecycleBinFolder
+end
+
 # Initialization
 starship init fish | source
 zoxide init fish | source
-source ~/.fzf.fish
 
 # Aliases
 alias cat="batcat --theme=GitHub"
 alias l="ls -la"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias cd="z"
-alias refresh="source ~/.config/fish/config.fish; echo 'Fish config refreshed!!'"
 
+alias refresh="source ~/.config/fish/config.fish; echo 'Fish config refreshed!!'"
 alias github="z d github"
+alias trash="recicle_bin_folder"
 
 # FZF
 set -x FZF_CTRL_T_OPTS "--style full --walker-skip .git,/node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
@@ -22,3 +28,4 @@ set -x FZF_CTRL_T_OPTS "--style full --walker-skip .git,/node_modules,target --p
 function fish_user_key_bindings
     bind \ct "fzf-tmux -- --preview 'bat -n --color=always {}'"
 end
+
